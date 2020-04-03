19479e0c6bc903ee96bf7efa69b57185b5e84242
JaeZheng <846265697@qq.com>
2020-04-03 16:03:22 +0800

    修改第12题，增加快速幂

--- 12.py
+++ 12.py
@@ -2,6 +2,8 @@
 题目描述:
 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
 保证base和exponent不同时为0
+解题思路：
+要考虑多种情况
 """
 
 # -*- coding:utf-8 -*-
@@ -11,7 +13,22 @@ class Solution:
             return 0
         if exponent == 0:
             return 1
-        return base ** int(exponent)
+        # return base ** int(exponent)
+        return self.quick_pow(base, exponent)
+
+    def quick_pow(self, base, exp):
+        flag = 1
+        re = 1
+        tmp = base
+        if exp < 0:
+            flag = 0
+            exp = abs(exp)
+        while exp > 0:
+            if exp & 1 == 1:
+                re = re * tmp
+            tmp = tmp * tmp
+            exp >>= 1
+        return re if flag else 1/re
 
 S = Solution()
 base, exponent = 2.0, 3.2
